13d82524d1ed5c6cfed5bf21768800f4585fc61a
Rakesh Ravuri <rravuri@gmail.com>
2019-05-18 02:33:15 +0530

    added secrets to build

--- index.js
+++ index.js
@@ -1,11 +1,13 @@
 const express = require('express');
+const fs = require('fs');
 const app = express();
 
+app.secrets = JSON.parse(fs.readFileSync('secrets.json'));  
 app.get('/', (req, res) => {
   console.log('psp received a request.');
 
   const target = process.env.TARGET || 'World';
-  res.send(`Hello ${target}!`);
+  res.send(`Hello ${target}!\nDB:${app.secrets.DB}`);
 });
 
 const port = process.env.PORT || 8080;
