47c12262490ef6ce403fd2d54970c3e869ec5b24
Rakesh Ravuri <rravuri@gmail.com>
2019-09-19 14:45:34 +0530

    registeredUsersCount

--- client/src/domain/Data.js
+++ client/src/domain/Data.js
@@ -5,6 +5,24 @@ import moment from 'moment';
 const dataService = new APIResource({resourceName:'data'})
 
 export default class Data {
+  static async getUserSummary() {
+    try {
+      let url = `/u?v=1`;
+      // if (startDate) {
+      //   url+=`&startDate=${moment(startDate).format('YYYY-MM-DD')}`;
+      // }
+      // if (limit) {
+      //   url+=`&limit=${limit}`;
+      // }
+      const {data} = await dataService.fetch(url);
+      return data;
+
+    }catch (err) {
+      console.error(err);
+      throw new VError(err,'Unable to retrive summary of users.');
+    }
+  }
+
   static async getQuestionsSummary({startDate, limit}) {
     try {
       let url = `/q?v=1`;
